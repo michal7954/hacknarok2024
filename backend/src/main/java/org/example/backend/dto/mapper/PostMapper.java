f34757ebed6263d84b65839b4320796152130e3d
Patryk <patrykjanas27@gmail.com>
2024-04-06 19:29:52 +0200

    update

--- backend/src/main/java/org/example/backend/dto/mapper/PostMapper.java
+++ backend/src/main/java/org/example/backend/dto/mapper/PostMapper.java
@@ -1,12 +1,41 @@
 package org.example.backend.dto.mapper;
 
 import org.example.backend.db.Post;
+import org.example.backend.db.PostStatistic;
 import org.example.backend.dto.request.PostWriteDto;
+import org.example.backend.dto.respond.PostAndStatsReadDto;
 import org.example.backend.dto.respond.PostReadDto;
+import org.example.backend.dto.respond.PostStatisticReadDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface PostMapper {
     Post mapPostWriteDtoToPost(PostWriteDto postWriteDto);
     PostReadDto mapPostToReadDto(Post post);
+    @Mapping(source = "post.id", target = "id")
+    @Mapping(source = "postStatistic", target = "postStatisticReadDto", qualifiedByName = "postStatisticReadDtoMapping")
+    PostAndStatsReadDto mapToPostAndStatsReadDto(Post post, PostStatistic postStatistic);
+
+
+    @Named("postStatisticReadDtoMapping")
+    default PostStatisticReadDto map(PostStatistic postStatistic) {
+        if (postStatistic == null) {
+            return null;
+        }
+        return new PostStatisticReadDto(
+                postStatistic.getId(),
+                postStatistic.getNumberOfViews(),
+                postStatistic.getNumberOfLikes(),
+                postStatistic.getNumberOfComments(),
+                postStatistic.getNumberOfShares(),
+                postStatistic.getEngagementRate(),
+                postStatistic.getNumberOfImpressions(),
+                postStatistic.getReach(),
+                postStatistic.getClickThroughRate(),
+                postStatistic.getConversionRate(),
+                postStatistic.getAverageTimeSpent()
+        );
+    }
 }
