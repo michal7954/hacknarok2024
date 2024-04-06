183a6f622bb42811d7f7fe9f5a1a9c219937c338
Michał <mmichal7954@gmail.com>
2024-04-06 19:08:41 +0200

    Home navbar

--- frontend/src/features/home/Home.tsx
+++ frontend/src/features/home/Home.tsx
@@ -1,10 +1,35 @@
 import React from 'react';
 import './Home.scss';
+import { Avatar, Fab } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import AutoAwesomeIcon from '@mui/icons-material/AutoAwesome';
+import AddIcon from '@mui/icons-material/Add';
 
 export default function Home() {
   return (
     <div className="home">
-      Home
+      <nav className="nav">
+        <Avatar alt="Avatar" variant="circular" className="avatar">
+          <PersonIcon />
+        </Avatar>
+        <Fab
+          variant="extended"
+          color="default"
+          size="large"
+        >
+          ADD EXISTING POST <AddIcon />
+        </Fab>
+        <Fab
+          variant="extended"
+          color="default"
+          size="large"
+        >
+          GENERATE NEW POST <AutoAwesomeIcon />
+        </Fab>
+      </nav>
+      <div className="posts">
+        Posts list
+      </div>
     </div>
   );
 }
