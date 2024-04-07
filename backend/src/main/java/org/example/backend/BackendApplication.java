89d7cf840c48091cd05991c5242a714d3520f674
Patryk <patrykjanas27@gmail.com>
2024-04-07 07:45:00 +0200

    update samples data

--- backend/src/main/java/org/example/backend/BackendApplication.java
+++ backend/src/main/java/org/example/backend/BackendApplication.java
@@ -30,18 +30,18 @@ public class BackendApplication {
 
     public static void initPersonsToDatabase(ConfigurableApplicationContext context) {
         var personRepository = context.getBean(AccountRepository.class);
-        for (int i = 0; i < NUMBER_OF_ACCOUNTS; i++) {
-            Account account = generateRandomPerson(context);
+
+            Account account = generatePerson(context);
             personRepository.save(account);
-        }
+
     }
 
-    public static Account generateRandomPerson(ConfigurableApplicationContext context) {
+    public static Account generatePerson(ConfigurableApplicationContext context) {
         Account account = new Account();
-        account.setName("Name " + random.nextInt(20));
-        account.setAge(random.nextInt(50));
+        account.setName("John");
+        account.setAge(23);
 //        account.setNumberOfPosts(NUMBER_OF_POSTS_PER_ACCOUNT);
-        account.setNumberOfFollowers(random.nextInt(1000));
+        account.setNumberOfFollowers(1300);
 //        account.setNumberOfViews(random.nextInt(10000));
 //        account.setNumberOfComments(random.nextInt(1000));
 //        account.setNumberOfShares(random.nextInt(500));
@@ -52,46 +52,172 @@ public class BackendApplication {
     }
 
 
-    public static List<Post> generateRandomPosts(ConfigurableApplicationContext context) {
-        var postRepository = context.getBean(PostRepository.class);
-        List<Post> posts = new ArrayList<>();
-        for (int i = 0; i < NUMBER_OF_POSTS_PER_ACCOUNT; i++) {
-            PostStatistic postStatistic = initPostStatisticsToDatabase(context);
-            Post post = generateRandomPost();
-            post.setPostStatistic(postStatistic);
-            posts.add(postRepository.save(post));
-        }
-        return posts;
-    }
+//    public static List<Post> generateRandomPosts(ConfigurableApplicationContext context) {
+//        var postRepository = context.getBean(PostRepository.class);
+//        List<Post> posts = new ArrayList<>();
+//        for (int i = 0; i < NUMBER_OF_POSTS_PER_ACCOUNT; i++) {
+//            PostStatistic postStatistic = initPostStatisticsToDatabase(context);
+//            Post post = generateRandomPost();
+//            post.setPostStatistic(postStatistic);
+//            posts.add(postRepository.save(post));
+//        }
+//        return posts;
+//    }
 
     public static List<Post> generatePosts(ConfigurableApplicationContext context) {
         var postRepository = context.getBean(PostRepository.class);
+        var postStatisticRepository = context.getBean(PostStatisticRepository.class);
         List<Post> posts = new ArrayList<>();
         Post post1 = new Post();
         post1.setContent("Yesterday we took part in the competition of students' innovations using IT at AGH University of Science and Technology and our project was awarded by organizers and sponsors of hashtag#ITisMe conference. The project was received as useful in everyday life and with commercial potential. \"RentMe\" is for lending, inventorying and item management. Thank you for this opportunity to participate in such a great festival.");
-        post1.setKeywords(Arrays.asList("Keyword " + random.nextInt(20),
-                "Keyword " + random.nextInt(20),
-                "Keyword " + random.nextInt(20)));
-        post1.setAuthor("Author");
-        post1.setPostStatistic(initPostStatisticsToDatabase(context));
+        post1.setKeywords(Arrays.asList("AGH", "Innovation"));
+        post1.setAuthor("John");
+        PostStatistic postStatistic1 = new PostStatistic();
+        postStatistic1.setNumberOfViews(800);
+        postStatistic1.setNumberOfLikes(350);
+        postStatistic1.setNumberOfComments(120);
+        postStatistic1.setNumberOfShares(60);
+        postStatistic1.setEngagementRate(4.0);
+        postStatistic1.setNumberOfImpressions(1100);
+        postStatistic1.setReach(800);
+        postStatistic1.setClickThroughRate(1.8);
+        postStatistic1.setConversionRate(0.4);
+        postStatistic1.setAverageTimeSpent(32.5);
+        post1.setPostStatistic(postStatisticRepository.save(postStatistic1));
 
         Post post2 = new Post();
         post2.setContent("November 19-20, our team of 6 members took part in a hashtag#HackYeah2022 in Tauron Arena Kraków, we had been working 24 hours on a specific challenge and trying to solve it in the most effective way. We focused on Let's compost IT task. During 24 hours of intensive work, thanks to effective cooperation with Adam Wasylewicz on server-side application, we have done 65 commits, and we created a minimum viable product. Mariusz helped us with quick deployment to deliver applications in proper condition. Karolina made excellent design of our app, whereas frontend developers made client-side application based on created by Karolina design. Great atmosphere and new experience for us.");
-        post2.setKeywords(Arrays.asList("Keyword " + random.nextInt(20),
-                "Keyword " + random.nextInt(20),
-                "Keyword " + random.nextInt(20)));
-        post2.setAuthor("Author");
-        post2.setPostStatistic(initPostStatisticsToDatabase(context));
+        post2.setKeywords(Arrays.asList("HackYeah2022", "Kraków"));
+        post2.setAuthor("John");
+        PostStatistic postStatistic2 = new PostStatistic();
+        postStatistic2.setNumberOfViews(900);
+        postStatistic2.setNumberOfLikes(400);
+        postStatistic2.setNumberOfComments(150);
+        postStatistic2.setNumberOfShares(70);
+        postStatistic2.setEngagementRate(4.5);
+        postStatistic2.setNumberOfImpressions(1200);
+        postStatistic2.setReach(850);
+        postStatistic2.setClickThroughRate(2.0);
+        postStatistic2.setConversionRate(0.45);
+        postStatistic2.setAverageTimeSpent(35.0);
+        post2.setPostStatistic(postStatisticRepository.save(postStatistic2));
+
         Post post3 = new Post();
         post3.setContent("On December 9, 2022, we participate in the St. Barbara's day conference of AHG University of Science and Technology. This is one of the largest scientific event at the AGH, and it was an opportunity to present our scientific work. We were the 3rd place winners in the Computer Science section.");
-        post3.setKeywords(Arrays.asList("Keyword " + random.nextInt(20),
-                "Keyword " + random.nextInt(20),
-                "Keyword " + random.nextInt(20)));
-        post3.setAuthor("Author");
-        post3.setPostStatistic(initPostStatisticsToDatabase(context));
+        post3.setKeywords(Arrays.asList("AGH", "Scientific Event"));
+        post3.setAuthor("John");
+        PostStatistic postStatistic3 = new PostStatistic();
+        postStatistic3.setNumberOfViews((int) (800 * 1.5));
+        postStatistic3.setNumberOfLikes((int) (350 * 1.5));
+        postStatistic3.setNumberOfComments((int) (120 * 1.5));
+        postStatistic3.setNumberOfShares((int) (60 * 1.5));
+        postStatistic3.setEngagementRate(4.0 * 1.5);
+        postStatistic3.setNumberOfImpressions((int) (1100 * 1.5));
+        postStatistic3.setReach((int) (800 * 1.5));
+        postStatistic3.setClickThroughRate(1.8 * 1.5);
+        postStatistic3.setConversionRate(0.4 * 1.5);
+        postStatistic3.setAverageTimeSpent(32.5 * 1.5);
+        post3.setPostStatistic(postStatisticRepository.save(postStatistic3));
+
+
+        Post post4 = new Post();
+        post4.setContent("Hey there, fellow corporate warriors! \uD83D\uDCBC\uD83D\uDD25\n" +
+                "\n" +
+                "Let's talk about something that affects all of us, no matter how busy our schedules get – our health and diet! \uD83E\uDD66\uD83C\uDFCB\uFE0F\u200D♀\uFE0F\n" +
+                "\n" +
+                "As professionals, we often find ourselves caught up in the demands of our work, leaving little time to focus on our well-being. But let me tell you, taking care of your health is not a luxury, it's an essential investment! \uD83D\uDCAA\uD83D\uDCAF\n" +
+                "\n" +
+                "A healthy diet can fuel your mind, boost your productivity, and keep you performing at your best in the corporate world. So why not prioritize it, starting today? \uD83C\uDF1F\n" +
+                "\n" +
+                "Remember, it's all about balance and making smart choices. Incorporating nutritious meals, regular exercise, and self-care into our daily routines can do wonders for our energy levels, mental clarity, and overall happiness. \uD83C\uDF08✨\n" +
+                "\n" +
+                "Let's inspire and support each other on this journey to a healthier lifestyle. Share your favorite quick and nutritious recipes, fitness tips, or even the challenges you face in prioritizing your well-being. Together, we can create a community that embraces and thrives on health and wellness! \uD83C\uDF31\uD83E\uDD1D\n" +
+                "\n" +
+                "So what are you waiting for? Join the conversation and let's conquer the corporate world with vibrant energy and a nourished body and mind! \uD83D\uDCA5\uD83D\uDCBC");
+        post4.setKeywords(Arrays.asList("healthanddiet ", "corporatewellness ", "prioritizeyourwellbeing ", "mindbodysoul ", "healthychoices"));
+        post4.setAuthor("Mati");
+        PostStatistic postStatistic4 = new PostStatistic();
+        postStatistic4.setNumberOfViews((int) (800 * 1.7));
+        postStatistic4.setNumberOfLikes((int) (350 * 1.7));
+        postStatistic4.setNumberOfComments((int) (120 * 1.7));
+        postStatistic4.setNumberOfShares((int) (60 * 1.7));
+        postStatistic4.setEngagementRate(4.0 * 1.7);
+        postStatistic4.setNumberOfImpressions((int) (1100 * 1.7));
+        postStatistic4.setReach((int) (800 * 1.7));
+        postStatistic4.setClickThroughRate(1.8 * 1.7);
+        postStatistic4.setConversionRate(0.4 * 1.7);
+        postStatistic4.setAverageTimeSpent(32.5 * 1.7);
+        post4.setPostStatistic(postStatisticRepository.save(postStatistic4));
+
+        Post post5 = new Post();
+        post5.setContent("\uD83C\uDF31 Boost Your Performance at Work with a Healthy Diet \uD83C\uDF31\n" +
+                "\n" +
+                "Are you feeling drained and lacking the energy to conquer your workday? It's time to supercharge your performance with the power of a healthy diet!\n" +
+                "\n" +
+                "As professionals in the corporate world, we often find ourselves caught up in the hustle and bustle, neglecting our own health and well-being. But did you know that what you eat can have a significant impact on your productivity and focus?\n" +
+                "\n" +
+                "Investing in a balanced diet will not only enhance your physical health but also sharpen your mental clarity. Incorporating nutrient-rich foods such as fruits, vegetables, lean proteins, and whole grains can provide the necessary fuel to power through your busiest days.\n" +
+                "\n" +
+                "Moreover, a healthy diet can help you manage stress more effectively, improve your mood, and boost your immune system, reducing sick days and increasing your overall effectiveness at work.\n" +
+                "\n" +
+                "So, let's commit to making smart choices when it comes to what we put on our plates. Start small by swapping out that greasy takeout for a delicious homemade meal or opting for a nutritious snack instead of sugary treats.\n" +
+                "\n" +
+                "Remember, your health is your greatest asset, and by prioritizing it, you'll not only improve your performance at work but also enhance your overall quality of life.\n" +
+                "\n" +
+                "\uD83E\uDD57 Fuel your success with a healthy diet and watch your productivity soar! \uD83D\uDE80");
+        post5.setKeywords(Arrays.asList("HealthyLifestyle  ", "CorporateWellness  ", "HealthyEating  ", "ProductivityBoost "));
+        post5.setAuthor("Mati");
+        PostStatistic postStatistic5 = new PostStatistic();
+        postStatistic5.setNumberOfViews((int) (800 * 1.9));
+        postStatistic5.setNumberOfLikes((int) (350 * 1.9));
+        postStatistic5.setNumberOfComments((int) (120 * 1.9));
+        postStatistic5.setNumberOfShares((int) (60 * 1.9));
+        postStatistic5.setEngagementRate(4.0 * 1.9);
+        postStatistic5.setNumberOfImpressions((int) (1100 * 1.9));
+        postStatistic5.setReach((int) (800 * 1.9));
+        postStatistic5.setClickThroughRate(1.8 * 1.9);
+        postStatistic5.setConversionRate(0.4 * 1.9);
+        postStatistic5.setAverageTimeSpent(32.5 * 1.9);
+        post5.setPostStatistic(postStatisticRepository.save(postStatistic5));
+
+        Post post6 = new Post();
+        post6.setContent("Hello, fellow professionals! \uD83D\uDCBC\n" +
+                "\n" +
+                "In today's fast-paced corporate world, our health and well-being often take a backseat. But let's change that together! \uD83D\uDCAA\n" +
+                "\n" +
+                "Did you know that maintaining a healthy diet can significantly boost your productivity, focus, and overall success? \uD83E\uDD66\uD83C\uDF4E\n" +
+                "\n" +
+                "It's time to fuel our bodies with nutritious meals and snacks that will keep us energized throughout the day. \uD83C\uDF31 Whether it's packing a homemade lunch or opting for healthy options during business meetings, small changes can make a big difference in our performance.\n" +
+                "\n" +
+                "Let's prioritize our health and make conscious choices to support our goals and ambitions. \uD83C\uDF1F Remember, a healthy body leads to a healthy mind, and ultimately, a thriving career! \uD83D\uDE4C\n" +
+                "\n" +
+                "Are you ready to take on the challenge and start incorporating healthier habits into your corporate lifestyle? Share your thoughts and ideas in the comments below! \uD83D\uDC47");
+        post6.setKeywords(Arrays.asList("HealthFirst ", "CorporateWellness ", "EnergizeYourDay"));
+        post6.setAuthor("Mati");
+        PostStatistic postStatistic6 = new PostStatistic();
+        postStatistic6.setNumberOfViews((int) (800 * 1.8));
+        postStatistic6.setNumberOfLikes((int) (350 * 1.8));
+        postStatistic6.setNumberOfComments((int) (120 * 1.8));
+        postStatistic6.setNumberOfShares((int) (60 * 1.8));
+        postStatistic6.setEngagementRate(4.0 * 1.8);
+        postStatistic6.setNumberOfImpressions((int) (1100 * 1.8));
+        postStatistic6.setReach((int) (800 * 1.8));
+        postStatistic6.setClickThroughRate(1.8 * 1.8);
+        postStatistic6.setConversionRate(0.4 * 1.8);
+        postStatistic6.setAverageTimeSpent(32.5 * 1.8);
+        post6.setPostStatistic(postStatisticRepository.save(postStatistic6));
+
+
+
+
         posts.add(postRepository.save(post1));
         posts.add(postRepository.save(post2));
         posts.add(postRepository.save(post3));
+        posts.add(postRepository.save(post4));
+        posts.add(postRepository.save(post5));
+        posts.add(postRepository.save(post6));
+
+
 
         return posts;
     }
@@ -112,13 +238,13 @@ public class BackendApplication {
         return postStatisticRepository.save(postStatistic);
     }
 
-    public static Post generateRandomPost() {
-        Post post = new Post();
-        post.setContent("Random Content " + random.nextInt(1000));
-        post.setKeywords(Arrays.asList("Keyword " + random.nextInt(20),
-                "Keyword " + random.nextInt(20),
-                "Keyword " + random.nextInt(20)));
-        post.setAuthor("Author");
-        return post;
-    }
+//    public static Post generateRandomPost() {
+//        Post post = new Post();
+//        post.setContent("Random Content " + random.nextInt(1000));
+//        post.setKeywords(Arrays.asList("Keyword " + random.nextInt(20),
+//                "Keyword " + random.nextInt(20),
+//                "Keyword " + random.nextInt(20)));
+//        post.setAuthor("Author");
+//        return post;
+//    }
 }
